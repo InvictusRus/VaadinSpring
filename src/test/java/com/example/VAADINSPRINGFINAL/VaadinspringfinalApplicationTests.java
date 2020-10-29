d0de393e2f76c0f085303e13ada86afce90fd0d6
InvictusRus <oldgoldenseye@gmail.com>
2020-10-29 11:06:31 +0700

    123

---
+++ src/test/java/com/example/VAADINSPRINGFINAL/VaadinspringfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VAADINSPRINGFINAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinspringfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
